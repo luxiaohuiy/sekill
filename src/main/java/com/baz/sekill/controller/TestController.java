ccea1feab963c6c89c9f6cd530d0907fb081e58f
xhlu <luxiaohuiy@163.com>
2019-04-12 20:54:32 +0800

    添加测试代码

---
+++ src/main/java/com/baz/sekill/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.baz.sekill.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+    @RequestMapping("/hell")
+    public Object test(){
+        return "hello";
+    }
+
+}
