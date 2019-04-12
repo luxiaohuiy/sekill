af5c14448ee7d33a06147a98158537ffad796abf
xhlu <luxiaohuiy@163.com>
2019-04-12 17:05:40 +0800

    创建项目

---
+++ src/main/java/com/baz/sekill/SekillApplication.java
@@ -0,0 +1,13 @@
+package com.baz.sekill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SekillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SekillApplication.class, args);
+    }
+
+}
