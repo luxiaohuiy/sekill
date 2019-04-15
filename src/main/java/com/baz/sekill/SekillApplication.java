0273555d826ced5ff3a43c5b39f583f81e52526f
xhlu <luxiaohuiy@163.com>
2019-04-15 10:29:18 +0800

    用generator生成实体类，xml文件

--- src/main/java/com/baz/sekill/SekillApplication.java
+++ src/main/java/com/baz/sekill/SekillApplication.java
@@ -1,9 +1,11 @@
 package com.baz.sekill;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.baz.sekill.mapper")
 public class SekillApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/baz/sekill/SekillApplicationTests.java
+++ src/test/java/com/baz/sekill/SekillApplicationTests.java
@@ -1,16 +1,31 @@
 package com.baz.sekill;
 
+import com.baz.sekill.mapper.SkBillMapper;
+import com.baz.sekill.model.SkBill;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+import java.util.Date;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SekillApplicationTests {
 
+    @Resource
+    private SkBillMapper skBillMapper;
+
     @Test
     public void contextLoads() {
+        SkBill skBill = new SkBill();
+        skBill.setUser_id(1);
+        skBill.setGoods_id(1);
+        skBill.setPay_status(1);
+        skBill.setUpdate_time(new Date());
+        skBill.setCreat_time(new Date());
+        skBillMapper.insert(skBill);
     }
 
 }
